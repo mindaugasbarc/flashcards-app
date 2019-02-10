07ab402dbc39a59136be0d706b9953951e29e37a
Mindaugas <mindaugas.barcauskas@mif.stud.vu.lt>
2019-02-10 13:23:56 +0200

    adding actions, reducers folders with index files

---
+++ src/components/app.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+    render() {
+        return (
+            <div>
+                <h1>This app is working</h1>
+            </div>
+        );
+    }
+}
